f53d91780840747b34c4a4fb33d6ec51ea496757
Sahlin <devsahlin@gmail.com>
2024-05-08 14:16:06 +0200

    Change tailwindcss for scss

--- client/src/modules/views/Home/Home.tsx
+++ client/src/modules/views/Home/Home.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Home.css';
+import { Button } from '../common/Button';
 
 const Home: React.FC = () => {
 	return (
 		<>
-			<div className='p-4 max-w-md mx-auto mt-10 bg-white rounded-xl shadow-md flex items-center space-x-4'>
-				<div>
-					<div className='text-xl font-medium text-black'>
-						Welcome to Tailwind!
-					</div>
-					<p className='text-gray-500'>
-						You're all set to start using Tailwind CSS in your React TypeScript
-						project.
-					</p>
+			<header></header>
+			<nav id='navbar'>
+				<div className='logo-container'>
+					<img id='logo'></img>
 				</div>
-			</div>
+				<ul id='menu--primary'>
+					<li></li>
+				</ul>
+				<Button />
+			</nav>
+			<section></section>
+			<main></main>
+			<footer></footer>
 		</>
 	);
 };
